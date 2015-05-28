ed3205ee613bc66b8c16f11e42af2511348f84f9
ProZsolt <prozsolt@gmail.com>
2015-05-28 02:40:11 +0200

    fix

--- puppet/modules/drupal/manifests/init.pp
+++ puppet/modules/drupal/manifests/init.pp
@@ -3,6 +3,7 @@ class drupal {
   exec { 'apt-update':
     command => '/usr/bin/apt-get update'
   }
+  
   # install drush  package
   package { 'drush':
     require => Exec['apt-update'],
@@ -14,13 +15,17 @@ class drupal {
     require => Exec['apt-update'],
     ensure => installed,
   }
+  
   # downloading drupal
   exec { 'install-drupal':
     cwd => '/var/www',
-    command => '/usr/bin/drush -y dl drupal --drupal-project-rename=drupal',
-    creates => '/var/www/drupal',
-    require => Package['drush']
+    command => '/usr/bin/drush -y dl drupal --destination=/var/www --drupal-project-rename=drupal',
+    require => [ Package['drush'],
+                 Class['apache']
+               ],
+    logoutput => on_failure,
   }
+  
   # install drupal site
   define site( $password, $dbname) {
     include drupal
@@ -35,8 +40,8 @@ class drupal {
     # install Drupal site
     exec { "site-install-${name}":
       cwd => "/var/www/drupal",
-      command => "/usr/bin/drush -y site-install standard --site-name=${name} --db-url=mysql://${dbname}:${password}@localhost/${dbname} --account-name=admin --account-pass=${password}",
-      require => [ Exec["install-drupal"],
+      command => "/usr/bin/drush -y -r /var/www/drupal site-install standard --site-name=${name} --db-url=mysql://${dbname}:${password}@localhost/${dbname} --account-name=admin --account-pass=${password}",
+      require => [ Exec['install-drupal'],
                    Package['php5-gd'],
                    Mysql::Db[$dbname] 
                  ],
